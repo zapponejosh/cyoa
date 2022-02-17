ca71709a2a5821909b5c5a35e95b7a9c65d1abef
Josh Zappone <zappone.josh@gmail.com>
2022-02-16 21:49:47 -0800

    setup server, add pure css

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/joshzappone/cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "Port to start the applicaion")
 	filename := flag.String("file", "gopher.json", "JSON file with CYOA story!")
 	flag.Parse()
 	file, err := os.Open(*filename)
@@ -21,6 +24,9 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
 
+	fmt.Printf("Starting server on http://localhost:%d\n", *port)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- story.go
+++ story.go
@@ -2,7 +2,9 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
 
 func JsonStory(r io.Reader) (Story, error) {
@@ -26,3 +28,50 @@ type Option struct {
 	Text    string `json:"text"`
 	Chapter string `json:"arc"`
 }
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<link rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/pure-min.css" integrity="sha384-Uu6IeWbM+gzNVXJcM9XV3SohHtmWE+3VGi496jvgX1jyvDTXfdK+rfZc8C1Aehk5" crossorigin="anonymous">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+	<div class="pure-g">
+    <h1 class="pure-u">{{.Title}}</h1>
+    {{ range .Paragraphs}}
+    <p class="pure-u">{{.}}</p>
+    {{end}}
+
+    <ul class="pure-u">
+      {{range .Options}}
+      <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+      {{end}}
+    </ul>
+		</div>
+  </body>
+</html>`
